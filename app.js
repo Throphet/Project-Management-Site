daadbfa050a3f736d17c69508960b0a328225614
Throphet <throphet@gmail.com>
2021-03-09 14:33:53 +0200

    Tutorial #3 done (first changes)

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let titles = document.getElementsByClassName('title');
 
-console.log(Array.isArray(titles));
-console.log(Array.isArray(Array.from(titles)))
+// console.log(Array.isArray(titles));
+// console.log(Array.isArray(Array.from(titles)))
 
 Array.from(titles).forEach(function(item) {
     console.log(item);
